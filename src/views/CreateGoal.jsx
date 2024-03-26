5e44df62e7ca6205100b194a8848ccaddd28c3cb
j-tran90 <johnt90@live.com>
2024-03-26 00:09:22 -0400

    fixed accordion

--- src/views/CreateGoal.jsx
+++ src/views/CreateGoal.jsx
@@ -1,11 +1,14 @@
 import Goal from "../components/Goal";
 import User from "../components/User";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function CreateGoal() {
+  const { currentUser } = useAuth(null);
   return (
     <>
       <User />
-      <h2>Get started, set your goals!</h2>
+      <h2>Hi, {currentUser.displayName || "Guest"}! </h2>
+      <h2>Get started with your goals</h2>
       <Goal />
     </>
   );
